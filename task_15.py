ae03226c0eb5ee3c671084da8dd926d86f86e638
tolstikov_ae <tolstikov_ae@edu.ru>
2020-03-05 17:31:26 +1000

    пп

--- task_15.py
+++ task_15.py
@@ -5,7 +5,23 @@ from pyrob.api import *
 
 @task
 def task_8_21():
-    pass
+    if wall_is_above() and wall_is_on_the_left():
+        while True:
+            if not wall_is_on_the_right():
+                move_right()
+            else:
+                break
+            if not wall_is_beneath():
+                move_down()
+            else:
+                break
+    elif wall_is_above and wall_is_on_the_right():
+        pass
+    elif wall_is_beneath and wall_is_on_the_left():
+        pass
+    else:
+        pass
+
 
 
 if __name__ == '__main__':
